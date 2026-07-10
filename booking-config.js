0f6c22cd2ba02862af5b4495be792e131c8a1365
IvanZhila <87499737+IvanZhilyaev@users.noreply.github.com>
2026-07-10 23:59:32 +0300

    Обновлён адрес онлайн-записи

--- booking-config.js
+++ booking-config.js
@@ -1,3 +1,3 @@
 // Адрес сервера онлайн-записи. Обновляется автоматически скриптом туннеля
 // (detailing-booking/tunnel.js) при запуске онлайн-записи в студии.
-window.BOOKING_API_URL = 'https://threaded-bits-tough-talent.trycloudflare.com';
+window.BOOKING_API_URL = 'https://constitute-llc-hills-ron.trycloudflare.com';
